6772d073b7003c523fd8b674d36255ec8ac30008
ColdWindScholar <3590361911@qq.com>
2025-02-07 19:46:44 +0800

    [Normal] Update Some Code

--- run.py
+++ run.py
@@ -165,7 +165,6 @@ class set_utils:
             data = json.load(ss)
             [setattr(self, v, data[v]) for v in data]
         self.language_dict = getattr(languages, settings.language)
-        print("Building Language Map.")
         def sys_stdout_write(s:str):
             front = ''
             end = ''
@@ -174,6 +173,9 @@ class set_utils:
                 end = s[-4:]
                 s = s[5:-4]
             s_t = s.strip()
+            for i in self.language_dict:
+                if i in s:
+                    s = s.replace(i, self.language_dict.get(i, i))
             if s_t in self.language_dict.keys():
                 s = s.replace(s_t, self.language_dict.get(s_t, s_t))
             s = self.language_dict.get(s, s)
--- src/languages.py
+++ src/languages.py
@@ -3,5 +3,5 @@ Chinese:dict = {
     "Cannot Remove, PermissionError":"无法删除，权限不足",
     "Remove Successful! ":"删除成功！",
     "Input error!":"输入错误！",
-    "Alpha Edition":" 阿尔法版本 "
+    "Alpha Edition":"阿尔法版本"
 }
