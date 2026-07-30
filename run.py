844e02483f241e3277e97a4576a6f9981a1a3fde
ColdWindScholar <3590361911@qq.com>
2026-07-30 11:50:55 +0800

    add mcp support

--- run.py
+++ run.py
@@ -608,7 +608,7 @@ class Tool:
             self.setup_mcp()
         cls()
         if settings.banner != "6":
-            print(f'\033[31m {getattr(banner, "banner%s" % settings.banner)} \033[0m')
+            print(f'\033[31m {getattr(banner, f"banner{settings.banner}")} \033[0m')
         else:
             print("=" * 50)
         print("\033[93;44m Alpha Edition \033[0m")
